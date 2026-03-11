1972270ffe12400e41807ac5e8ff77ec6c824d62
Alex Villablanca <alex@villablanca.tech>
2026-03-10 20:40:11 -0400

    play notification after break

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,20 @@ use inquire::Select;
 use notify_rust::Notification;
 use std::{io::Cursor, process::exit, thread::sleep, time::Duration};
 
-fn main() {
-    let work_complete = Notification::new()
-        .summary("Pomodoro")
-        .body("Work Done")
-        .finalize();
-    let break_done = Notification::new()
-        .summary("Pomodoro")
-        .body("Break Over")
-        .finalize();
+fn notify_user(stream: &rodio::OutputStream, alert_file: &'static [u8], body: &str) {
+    let _ = Notification::new().summary("Pomodoro").body(body).show();
 
+    let sink = rodio::Sink::connect_new(stream.mixer());
+    sink.set_volume(4.0);
+    sink.append(rodio::Decoder::new(Cursor::new(alert_file)).unwrap());
+    sink.sleep_until_end();
+}
+
+fn main() {
     let stream_handle = rodio::OutputStreamBuilder::open_default_stream()
         .expect("Default audio stream unable to open");
 
     let alert_file = include_bytes!("alert.mp3");
-    let sink = rodio::Sink::connect_new(stream_handle.mixer());
     loop {
         let options = vec!["25m/5m", "50m/10m", "all done"];
         let time_choice = Select::new("Split", options).prompt();
@@ -39,10 +38,7 @@ fn main() {
             sleep(Duration::from_secs(1));
         }
         pb.finish_and_clear();
-        _ = work_complete.show();
-        sink.append(rodio::Decoder::new(Cursor::new(alert_file)).unwrap());
-        sink.set_volume(4.0);
-        sink.sleep_until_end();
+        notify_user(&stream_handle, alert_file, "Work Done");
 
         let options = vec!["Yes", "No"];
         let confirm = Select::new("Ready for a break?", options).prompt();
@@ -69,9 +65,6 @@ fn main() {
             sleep(Duration::from_secs(1));
         }
         pb.finish_and_clear();
-        _ = break_done.show();
-        sink.append(rodio::Decoder::new(Cursor::new(alert_file)).unwrap());
-        sink.set_volume(4.0);
-        sink.sleep_until_end();
+        notify_user(&stream_handle, alert_file, "Break Over");
     }
 }
